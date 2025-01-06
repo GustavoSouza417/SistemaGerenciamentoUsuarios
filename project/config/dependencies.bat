9831505f5eb38fc3f5875bbe67e239c654707cf6
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-06 10:09:55 -0300

    Add dependencies

---
+++ project/config/dependencies.bat
@@ -0,0 +1,15 @@
+:  PROJECT DEPENDENCIES
+
+: GraphQL
+npm install -s graphql;
+npm install -s graphql-tools;
+npm install -s @graphql-tools/load-files;
+npm install -s @graphql-tools/utils;
+
+: Apollo Server
+npm install -s apollo-server;
+
+: TypeScript
+npm install -s typescript;
+npm install --save-dev typescript @types/node;
+npm install -s ts-node;
