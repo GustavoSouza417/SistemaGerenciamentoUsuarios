4a1fd33ee4a71ee9ee1b0d894c60a7cae8ecbdfe
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-07 19:26:51 -0300

    Add UserService

---
+++ project/src/service/UserService.ts
@@ -0,0 +1,10 @@
+import User from "../type/class/User";
+// import UserValidator from "../validator/UserValidator";
+
+export default class UserService {
+    public static listUser(id: string): User | null {
+        
+        id;
+        return null;
+    };
+};
