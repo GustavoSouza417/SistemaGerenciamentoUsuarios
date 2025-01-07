4d8fdf77ee1d068b358288130d53d6fd87605193
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-07 11:15:27 -0300

    Change line

--- project/src/type/interface/user/User.ts
+++ project/src/type/interface/user/User.ts
@@ -1,9 +1,9 @@
 export default interface iUser {
     id: string;
-    profile: string;
     cpf: string;
     email: string;
     name: string;
     dateBirth: string;
     password: string;
+    profile: string;
 };
