10bf341c05e2963a9b4086f536717885316dfd51
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-07 10:24:29 -0300

    Add new errors

--- project/src/type/enum/Errors.ts
+++ project/src/type/enum/Errors.ts
@@ -1,4 +1,11 @@
 export enum Errors {
+    //model
     ERROR_READING_DATABASE = "An error occurred while reading the database",
-    ERROR_SAVING_DATA = "An error occured while saving the data to the database"
+    ERROR_SAVING_DATA = "An error occured while saving the data to the database",
+
+    //validator
+    ERROR_INVALID_ID = "The id received is invalid",
+    ERROR_INVALID_PROFILE = "The profile received is invalid",
+    ERROR_INVALID_CPF = "The CPF received is invalid",
+    ERROR_INVALID_EMAIL = "The email received is invalid"
 };
