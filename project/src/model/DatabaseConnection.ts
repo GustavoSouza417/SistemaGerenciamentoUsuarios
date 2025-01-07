8358b668cf6830c5d11fafc71349f86dcb6f85bd
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-07 19:29:38 -0300

    Directory update
    The directory of the "node:fs" dependency and the enum Errors was updated.

--- project/src/model/DatabaseConnection.ts
+++ project/src/model/DatabaseConnection.ts
@@ -1,6 +1,6 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import iDatabase from "../type/interface/database/Database";
-import { Errors } from "../type/enum/Errors";
+import { Errors } from "../type/enum/Errors/Errors";
 
 export default class DatabaseConnection {
     private static readonly url: string = "./src/model/database.json";
