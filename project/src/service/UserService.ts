d45e6b82708c565851968aa0e04767164cb410ba
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-08 12:57:42 -0300

    he listUser method has been completed

--- project/src/service/UserService.ts
+++ project/src/service/UserService.ts
@@ -1,10 +1,11 @@
-import User from "../type/class/User.js";
 import UserValidator from "../validator/UserValidator.js";
+import UserDAO from "../dao/UserDAO.js";
+// import User from "../type/class/User.js";
+import iUser from "../type/interface/user/User.js";
 
 export default class UserService {
-    public static listUser(id: string): User | null {
+    public static listUser(id: string): iUser | null {
         UserValidator.isIdValid(id);
-        
-        return null;
+        return UserDAO.listUser(id.trim());
     };
 };
