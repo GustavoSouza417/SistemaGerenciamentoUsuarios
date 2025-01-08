36faaf2beffb9cb9f3fc67e6b2085f4055811fc1
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-08 14:06:34 -0300

    Add features list users and list profile

--- project/src/graphql/modules/user/resolver.ts
+++ project/src/graphql/modules/user/resolver.ts
@@ -1,15 +1,22 @@
 import UserService from "../../../service/UserService.js";
-import User from "../../../type/class/User.js";
+// import User from "../../../type/class/User.js";
 import iUser from "../../../type/interface/user/User.js";
+import iProfile from "../../../type/interface/profile/Profile.js";
 
 export default {
+    User: {
+        profile(user: iUser): iProfile {
+            return UserService.listProfile(user);
+        }
+    },
+
     Query: {
         listUser(_: any, args: {id: string}): iUser | null {
             return UserService.listUser(args.id);
         },
 
-        listUsers(): User[] | null {
-            return null;
+        listUsers(): iUser[] {
+            return UserService.listUsers();
         }
     }
 };
