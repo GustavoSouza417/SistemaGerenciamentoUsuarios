82e6804acdc035281cfe84f9515326e425f3be4f
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-08 13:00:12 -0300

    Added parameters to listUser

--- project/src/graphql/modules/user/resolver.ts
+++ project/src/graphql/modules/user/resolver.ts
@@ -1,10 +1,11 @@
 import UserService from "../../../service/UserService.js";
 import User from "../../../type/class/User.js";
+import iUser from "../../../type/interface/user/User.js";
 
 export default {
     Query: {
-        listUser(id: string): User | null {
-            return UserService.listUser(id);
+        listUser(_: any, args: {id: string}): iUser | null {
+            return UserService.listUser(args.id);
         },
 
         listUsers(): User[] | null {
