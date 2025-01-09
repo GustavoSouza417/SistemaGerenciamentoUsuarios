225f4449b409ed0dda5031b054b051f9f6343243
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-09 17:36:39 -0300

    File path changed and completion of data existence check features

--- project/src/dao/user/UserDAO.ts
+++ project/src/dao/user/UserDAO.ts
@@ -1,7 +1,7 @@
-import DatabaseConnection from "../model/DatabaseConnection.js";
-import User from "../type/class/user/User.js";
-import iDatabase from "../type/interface/database/Database.js";
-import iUser from "../type/interface/user/User.js";
+import DatabaseConnection from "../../model/DatabaseConnection.js";
+import User from "../../type/class/user/User.js";
+import iDatabase from "../../type/interface/database/Database.js";
+import iUser from "../../type/interface/user/User.js";
 
 export default class UserDAO {
     public static create(user: User): void {
@@ -43,5 +43,29 @@ export default class UserDAO {
 
     public static deleteUsers(): void {
 
+    };
+
+    public static thisIdExists(id: string): boolean {
+        let json: iDatabase = DatabaseConnection.readJson();
+
+        return Object.values(json.users).some((user: iUser) => 
+            user.id === id
+        );
+    };
+
+    public static thisCpfExists(cpf: string): boolean {
+        let json: iDatabase = DatabaseConnection.readJson();
+
+        return Object.values(json.users).some((user: iUser) => 
+            user.cpf === cpf
+        );
+    };
+
+    public static thisEmailExists(email: string): boolean {
+        let json: iDatabase = DatabaseConnection.readJson();
+
+        return Object.values(json.users).some((user: iUser) => 
+            user.email === email
+        );
     };
 };
