32ef62c5b578d386193b9d67ea705771f2dd9769
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-01-09 17:38:44 -0300

    Corrections and new errors have been added

--- project/src/type/enum/Errors/Errors.ts
+++ project/src/type/enum/Errors/Errors.ts
@@ -3,12 +3,20 @@ export enum Errors {
     ERROR_READING_DATABASE = "An error occurred while reading the database",
     ERROR_SAVING_DATA = "An error occured while saving the data to the database",
 
-    //validator
-    ERROR_INVALID_ID = "The id received is invalid",
+    //user data validator
+    ERROR_INVALID_ID = "The ID received is invalid",
     ERROR_INVALID_CPF = "The CPF received is invalid",
     ERROR_INVALID_EMAIL = "The email received is invalid",
     ERROR_INVALID_NAME = "The name received is invalid",
     ERROR_INVALID_DATE_BIRTH = "The date birth received is invalid",
     ERROR_INVALID_PASSWORD = "The password received is invalid",
-    ERROR_INVALID_PROFILE = "The profile received is invalid"
+    ERROR_INVALID_PROFILE = "The profile received is invalid",
+
+    //user exists validator
+    ERROR_REGISTERED_ID = "The received ID is already registered",
+    ERROR_REGISTERED_CPF = "The received CPF is already registered",
+    ERROR_REGISTERED_EMAIL = "The received email is already registered",
+    ERROR_NO_REGISTERED_ID = "The received ID is not registered",
+    ERROR_NO_REGISTERED_CPF = "The received CPF is not registered",
+    ERROR_NO_REGISTERED_EMAIL = "The received email is not registered"
 };
